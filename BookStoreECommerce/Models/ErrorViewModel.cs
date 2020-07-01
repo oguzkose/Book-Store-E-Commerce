e51d322f2da6b7c0c04593d849dfc12ed89a7779
oguzkose <oguzkose2019@outlook.com>
2020-07-01 13:24:07 +0300

    Initial Commit

---
+++ BookStoreECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BookStoreECommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
