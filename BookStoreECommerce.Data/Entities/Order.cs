d75d1c31e38bd06353433ed37d1892997e77605f
fatih yılmaz <fatihksks_@hotmail.com>
2020-07-01 19:48:02 +0300

    Order table fix

--- BookStoreECommerce.Data/Entities/Order.cs
+++ BookStoreECommerce.Data/Entities/Order.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookStoreECommerce.Data.Entities
 {
     public class Order
     {
+        [Key]
         public int Id { get; set; }
         public int ProductId { get; set; }
         public int UserId { get; set; }
