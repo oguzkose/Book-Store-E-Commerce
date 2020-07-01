dcbf300c4b8226c56e0a4576cefd6a614d144424
fatih yılmaz <fatihksks_@hotmail.com>
2020-07-01 18:21:35 +0300

    BookEcommerceDBContext bağlantısı yapıldı.

---
+++ BookStoreECommerce.Data/Context/BookEcommerceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreECommerce.Data.Context
+{
+    public class BookEcommerceDbContext:DbContext
+    {
+        public BookEcommerceDbContext(DbContextOptions<BookEcommerceDbContext> options):base(options)
+        {
+
+        }
+
+    }
+}
