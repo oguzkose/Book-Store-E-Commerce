2aa4a14ec1ecfb43e5c0532a546643188efc1bb2
fatih yılmaz <fatihksks_@hotmail.com>
2020-07-02 13:33:31 +0300

    book list

---
+++ BookStoreECommerce.Service/Services/ProductService.cs
@@ -0,0 +1,68 @@
+﻿using BookStoreECommerce.Data.Context;
+using BookStoreECommerce.Data.Entities;
+using BookStoreECommerce.Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace BookStoreECommerce.Service.Services
+{
+    public class ProductService
+    {
+        #region Injection
+        private readonly BookEcommerceDbContext _context;
+        public ProductService(BookEcommerceDbContext context)
+        {
+            _context = context;
+        } 
+        #endregion
+
+        #region Kitap Listesi
+        public List<BookDto> GetBooks()
+        {
+            var entities = _context.Product.Join(_context.Category,
+                product => product.CategoryId,
+                category => category.Id, 
+                (product, category) => new BookDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    BookPages = product.BookPages,
+                    BookPublisher = product.BookPublisher,
+                    AddDate = product.AddDate,
+                    CategoryId = product.CategoryId,
+                    DatePublished = product.DatePublished,
+                    Description = product.Description,
+                    Isbn = product.Isbn,
+                    Price = product.Price,
+                    ReviewCount = product.ReviewCount,
+                    CategoryName = category.Name
+                }).ToList();
+
+            foreach (var item in entities)
+            {
+                var Images = GetImages(item.Id);
+                item.BookImages = Images;
+            }
+            return entities;
+        }
+        #endregion
+
+        #region Kitap Resimleri
+        private List<BookImagesDto> GetImages(int productId)
+        {
+            return _context.Product.Join(_context.ProductImage,
+                product => product.Id,
+                productImage => productImage.ProductId,
+                (product, productImage) => new { product.Id, productImage }).Where(query => query.productImage.ProductId == productId).Select(result => new BookImagesDto
+                {
+                    Id = result.productImage.Id,
+                    ImagePath = result.productImage.ProductImagePath,
+                    ProductId = result.productImage.ProductId
+                }).ToList();
+        } 
+        #endregion
+    }
+}
---
+++ BookStoreECommerce/Models/BookVM.cs
@@ -0,0 +1,25 @@
+﻿using BookStoreECommerce.Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreECommerce.Models
+{
+    public class BookVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Isbn { get; set; }
+        public string Description { get; set; }
+        public int BookPages { get; set; }
+        public string BookPublisher { get; set; }
+        public DateTime DatePublished { get; set; }
+        public DateTime AddDate { get; set; }
+        public decimal Price { get; set; }
+        public int? ReviewCount { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<BookImagesDto> BookImages { get; set; }
+    }
+}
